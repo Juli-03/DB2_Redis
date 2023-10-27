5fa74a81c658eb2cd474b9c9e818f53b34754c77
TimmyDasBrot <tim.steiner01@web.de>
2023-10-27 12:28:35 +0200

    login and registration UI

--- Implementierung/chatApp.py
+++ Implementierung/chatApp.py
@@ -2,7 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__,static_folder='staticFiles')
 
+# route to registration form
+@app.route('/register')
+def register():
+    return render_template('registration.html')
+
+# route to login form -> is set to default route, so user has always to login first
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+# home route is the chatroom
+@app.route('/home')
 def home():
     return render_template('chat.html')
 
