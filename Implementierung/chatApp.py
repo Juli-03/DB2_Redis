1ac856b0269866b71f8285f6b1f85fd40f1f98ac
TimmyDasBrot <tim.steiner01@web.de>
2023-10-27 12:42:45 +0200

    redirect
    if values are put in the user is getting redirected to the chatroom. Currently without any form of validation and db!

--- Implementierung/chatApp.py
+++ Implementierung/chatApp.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__,static_folder='staticFiles')
 
 # route to registration form
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('registration.html')
+    if request.method == 'POST':
+        # Process the form data here
+        # Perform registration and any necessary validation
+
+        # If registration is successful, redirect the user to the "home" route
+        return redirect(url_for('home'))
+    else:
+        return render_template('registration.html')
 
 # route to login form -> is set to default route, so user has always to login first
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        # Process the form data here
+        # Perform login and any necessary validation
+
+        # If login is successful, redirect the user to the "home" route
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
 
 # home route is the chatroom
 @app.route('/home')
