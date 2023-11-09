e519687d873a63e31c14747a02420841fd7b78f5
MWERGEN <108718663+MWERGEN@users.noreply.github.com>
2023-11-09 15:01:58 +0100

    file header

--- Implementierung/chatApp.py
+++ Implementierung/chatApp.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+"""
+File: chatApp.py
+Author: Tim Steiner, Julian Bork, Felix Wilhelm, Marius Wergen
+Date: October 13, 2023
+Description: This script starts the whole Web Application.
+
+Usage:
+- Function main(): Starts the Web Application.
+
+- creates Flask app
+
+- registers blueprints
+
+- runs app
+"""
+
+# imports of used libraries
 from config import Config
 from routes.chat import chat_bp
 from routes.login import login_bp
 from routes.registration import registration_bp
+from flask import Flask, render_template, request, redirect, url_for
 
 # connection to database
 pool = Config.pool
